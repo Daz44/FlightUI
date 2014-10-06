39ef6f84047cce8bfae5033783c29563d4fd8730
Daz44 <whosdaz@gmail.com>
2014-10-06 23:20:43 +1030

    FLUI_GLOBAL

--- src/com/mallen/flightui/wrapper/FLUI_GLOBAL.java
+++ src/com/mallen/flightui/wrapper/FLUI_GLOBAL.java
@@ -22,23 +22,42 @@ public class FLUI_GLOBAL {
 				public void run(){
 					while(true){
 						pitch = (int) (FLUI_READER.getDouble(12144)+90);
-						roll = (int) FLUI_READER.getDouble(12152);
-						qnhAlt = FLUI_READER.getInt(FLUI_MEMORY.FSUIPC_LOOKUP.get("QNH_ALTITUDE"));
-						radAlt = (int) Math.round(FLUI_READER.getInt(FLUI_MEMORY.FSUIPC_LOOKUP.get("RADIO_ALTITUDE"))/65536*3.28);
-						
-						hdg =  (int) Math.round(360.0*FLUI_READER.getInt(0x0580)/(65536.0*65536.0));//Correcting the Heading Value given by FSUIPC
-						if(hdg < 0) hdg = 360+hdg;
-						gps_waypoint = (int) Math.toDegrees(FLUI_READER.getDouble(FLUI_MEMORY.FSUIPC_LOOKUP.get("GPS_WAYPOINT_HEADING")));
-						
-						indicatorSpeed = FLUI_READER.getInt(0x02B8)/128;
-						
-						
-						try {
-							
-						} catch(Exception e){e.printStackTrace();}
+						roll = (int) FLUI_READER.getDouble(12152);						
 					}
 				}
 			});
 			t.start();
+			
+			Thread t2 = new Thread(new Runnable(){
+				public void run(){
+					while(true){
+						qnhAlt = FLUI_READER.getInt(FLUI_MEMORY.FSUIPC_LOOKUP.get("QNH_ALTITUDE"));
+						radAlt = (int) Math.round(FLUI_READER.getInt(FLUI_MEMORY.FSUIPC_LOOKUP.get("RADIO_ALTITUDE"))/65536*3.28);
+					}
+				}
+			});
+			t2.start();
+			
+			Thread t3 = new Thread(new Runnable(){
+				public void run(){
+					while(true){
+						hdg =  (int) Math.round(360.0*FLUI_READER.getInt(0x0580)/(65536.0*65536.0));//Correcting the Heading Value given by FSUIPC
+						if(hdg < 0) hdg = 360+hdg;
+						gps_waypoint = (int) Math.toDegrees(FLUI_READER.getDouble(FLUI_MEMORY.FSUIPC_LOOKUP.get("GPS_WAYPOINT_HEADING")));
+					}
+				}
+			});
+			t3.start();
+			
+			Thread t4 = new Thread(new Runnable(){
+				public void run(){
+					while(true){
+						indicatorSpeed = FLUI_READER.getInt(0x02BC)/128;
+						trueSpeed =  FLUI_READER.getInt(0x02B8)/128;
+					}
+				}
+			});
+			t4.start();
+			
 	}
 }
