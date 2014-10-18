3c6e3412a6dad314bab3b885d27c51f3af091936
Daz44 <whosdaz@gmail.com>
2014-10-19 01:22:34 +1030

    Fixed error thrown by Radio
    Error would be flown when the aircraft wasn't initialised - error now
    caught throw value checking and will simply render 'OFF' instead of a
    frequency.

--- src/com/mallen/flightui/implementation/generic/panels/RadioPanel.java
+++ src/com/mallen/flightui/implementation/generic/panels/RadioPanel.java
@@ -11,7 +11,7 @@
 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 	THE SOFTWARE.
 
-*/
+ */
 
 package com.mallen.flightui.implementation.generic.panels;
 
@@ -25,52 +25,73 @@ import com.mallen.flightui.ui.modules.TextIndicatorRGB;
 import com.mallen.flightui.ui.modules.Theme;
 import com.mallen.flightui.wrapper.FLUI_GLOBAL;
 
-public class RadioPanel extends JPanel{
-	
+public class RadioPanel extends JPanel {
+
 	TextIndicatorRGB tiCom1 = new TextIndicatorRGB(10, 5, 200, 30, "COM1", 1);
 	TextIndicatorRGB tiCom2 = new TextIndicatorRGB(210, 5, 200, 30, "COM2", 1);
-	TextIndicatorRGB tiNav1 = new TextIndicatorRGB(410, 5, 200, 30, "LAND", 1);
-	TextIndicatorRGB tiNav2 = new TextIndicatorRGB(610, 5, 200, 30, "TAXI", 1);
-	
+	TextIndicatorRGB tiNav1 = new TextIndicatorRGB(410, 5, 200, 30, "NAV1", 1);
+	TextIndicatorRGB tiNav2 = new TextIndicatorRGB(610, 5, 200, 30, "NAV2", 1);
+
 	@Override
-	public void paintComponent(Graphics g){
-	super.paintComponent(g);
-	setDoubleBuffered(true);
-	
-	 Graphics2D g2d = (Graphics2D) g;
-	    RenderingHints rhints = g2d.getRenderingHints();
-	    rhints.containsValue(RenderingHints.VALUE_ANTIALIAS_ON);
-	    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-	
-	    try {
-		
-		    g.setColor(Theme.gBackground);
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		setDoubleBuffered(true);
+
+		Graphics2D g2d = (Graphics2D) g;
+		RenderingHints rhints = g2d.getRenderingHints();
+		rhints.containsValue(RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+
+		try {
+			g.setColor(Theme.gBackground);
 			g.fillRect(0, 0, getWidth(), getHeight());
-	
+
 			String commString = Integer.toHexString(FLUI_GLOBAL.COM1);
-			tiCom1.text = "COM1: 1" + commString.substring(0, 2) + "." + commString.substring(2, 4);
-			tiCom1.draw(g); 
-	
+			if (commString.length() >= 4) {
+				tiCom1.text = "COM1: 1" + commString.substring(0, 2) + "."
+						+ commString.substring(2, 4);
+			} else {
+				tiCom1.text = "COM1: OFF";
+			}
+			tiCom1.draw(g);
+
 			commString = Integer.toHexString(FLUI_GLOBAL.COM2);
-			tiCom2.text = "COM2: 1" + commString.substring(0, 2) + "." + commString.substring(2, 4);
+			if (commString.length() >= 4) {
+				tiCom2.text = "COM2: 1" + commString.substring(0, 2) + "."
+						+ commString.substring(2, 4);
+			} else {
+				tiCom2.text = "COM2: OFF";
+			}
 			tiCom2.draw(g);
-			
+
 			commString = Integer.toHexString(FLUI_GLOBAL.NAV1);
-			tiNav1.text = "NAV1: 1" + commString.substring(0, 2) + "." + commString.substring(2, 4);
+			if (commString.length() >= 4) {
+				tiNav1.text = "NAV1: 1" + commString.substring(0, 2) + "."
+						+ commString.substring(2, 4);
+			} else {
+				tiNav1.text = "NAV1: OFF";
+			}
 			tiNav1.draw(g);
-			
-			commString = Integer.toHexString(FLUI_GLOBAL.NAV2);
-			tiNav2.text = "NAV2: 1" + commString.substring(0, 2) + "." + commString.substring(2, 4);
+
+			if (commString.length() >= 4) {
+				commString = Integer.toHexString(FLUI_GLOBAL.NAV2);
+				tiNav2.text = "NAV2: 1" + commString.substring(0, 2) + "."
+						+ commString.substring(2, 4);
+
+			} else {
+				tiNav2.text = "NAV2: OFF";
+			}
 			tiNav2.draw(g);
-	    } catch(Exception e){
-	    	e.printStackTrace();
-	    }
-		
+		} catch (Exception e) {
+			System.err.println("Error in Radio");
+		}
+
 		try {
 			Thread.sleep(10);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}		
+		}
 		repaint();
 	}
 }
